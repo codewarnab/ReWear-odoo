56c084d08608af12f7d917f512b510e766f4b6d8
sutanukaa <sutanukachakraborty004@gmail.com>
2025-07-12 16:21:21 +0530

    categories and products added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import { HeroSection } from "@/components/home-page";
+import { HeroSection, CategoriesSection } from "@/components/home-page";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <HeroSection />
+      <CategoriesSection />
     </div>
   );
 }
